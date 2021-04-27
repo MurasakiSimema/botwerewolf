16a69cc7770a47d1ea43e46501d6fc235e96b857
Simone Bergonzi <simemablak@gmail.com>
2021-04-27 17:25:39 +0200

    port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const Database = require('better-sqlite3')
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ejs = require('ejs');
 
 const db = new Database('./BotDB.db', { verbose: console.log });
